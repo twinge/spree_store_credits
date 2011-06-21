94b6ad2e227854e40813783681848a202b7b9ebb
Josh Starcher <josh.starcher@gmail.com>
2011-06-21 12:16:47 -0400

    working on obscure error

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -4,7 +4,7 @@ CheckoutController.class_eval do
   private
   def remove_payments_attributes_if_total_is_zero
     return unless params[:order] && params[:order][:store_credit_amount]
-    store_credit_amount = [BigDecimal.new(params[:order][:store_credit_amount]), current_user.store_credits_total].min
+    store_credit_amount = [BigDecimal.new(params[:order][:store_credit_amount]), current_user.store_credits_total.to_i].min
     if store_credit_amount >= current_order.total
       params[:order].delete(:source_attributes)
       params.delete(:payment_source)
